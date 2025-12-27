5808d6dbe31c7b19dca245713720e8790347fe01
awanbiru2 <nanasetiawan0211@gmail.com>
2025-12-27 09:31:32 +0700

    [200~prefers-reduced-motion test page~

---
+++ Sesi - 04/script.js
@@ -0,0 +1,7 @@
+function redirInstagram() {
+  window.location.href = "https://www.instagram.com/nanastywn_/";
+}
+
+function redirGithub() {
+  window.location.href = "https://www.github.com/nanastywn/";
+}
