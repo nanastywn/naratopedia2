f652e0858c2f5e5132d00584ee87c8219cc300c5
awanbiru2 <nanasetiawan0211@gmail.com>
2026-01-15 15:33:14 +0700

    movie-web

--- Recap Sesi/S - 12/script.js
+++ Recap Sesi/S - 12/script.js
@@ -5,6 +5,7 @@ let page = 1;
 const API_URL = () =>
   `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=${page}`;
 const API_IMAGE_URL = "https://image.tmdb.org/t/p/w1280";
+const API_SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 async function getMovies(url) {
   const res = await fetch(url);
@@ -12,17 +13,24 @@ async function getMovies(url) {
   showMovies(data.results);
 }
 
+function updatePage() {
+  getMovies(API_URL());
+  currentPage.innerHTML = page;
+}
+
 function nextPage() {
   if (page >= 1) {
     page += 1;
-    gerMovies(API_URL());
+    getMovies(API_URL());
+    updatePage();
   }
 }
 
 function prevPage() {
   if (page > 1) {
-    page = 1;
+    page -= 1;
     getMovies(API_URL());
+    updatePage();
   }
 }
 
@@ -38,17 +46,32 @@ function showMovies(movies) {
   moviesElement.innerHTML = "";
   movies.forEach((movie) => {
     const { title, poster_path, overview, popularity, vote_average } = movie;
+    console.log({ popularity, vote_average });
     const movieCard = document.createElement("div");
     movieCard.classList.add("movie");
     movieCard.innerHTML = `
-    <img src="${API_IMAGE_URL + poster_path}" alt="HTML THE MOVIE IMAGE" />
+    <img src="${API_IMAGE_URL + poster_path}" alt="html the movie images" />
     <div class="detail">
-        <h3>${title}</h3>
-        <p>${overview.substring(0, 200)}...</p>
-        <p>${popularity} ⭐ | ${vote_average} 👍</p>
+        <h3 class = "judul" >${title}</h3>
+        <p class = "overview" >${overview.substring(0, 120)}...</p>
+        <p class="movieRating" >${popularity} ⭐ | ${vote_average} 👍</p>
     </div>`;
     moviesElement.appendChild(movieCard);
   });
 }
 
-getMovies(API_URL());
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchQuery = search.value;
+
+  if (searchQuery !== "") {
+    getMovies(API_SEARCH_URL + searchQuery);
+    search.value = "";
+  }
+});
+
+updatePage();
+
+title.addEventListener("click", () => {
+  location.reload();
+});
